ff87f674b0ca84fbea935ac0101a2e686ff2e804
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 12:12:14 -0300

    Add files via upload

---
+++ despesas.py
@@ -0,0 +1,18 @@
+# 6. Crie uma variável para calcular o gasto total dessa viagem considerando o preço da gasolina 5.85
+
+# distancia = 230
+# consumoVeiculo = 10
+# consumoViagem = distancia / consumoVeiculo
+# print(f"Você gastou {consumoViagem} litros")
+
+# precoGasolina = 5.85
+# custoViagem = consumoViagem * precoGasolina
+# print(f"Você gastou R${custoViagem:.2f}")
+
+
+consumo = 105 
+gasolina = 5.85
+
+despesas = consumo * gasolina
+
+print(f'A despesa com gasolina foi: R$ {despesas:.2f}')
